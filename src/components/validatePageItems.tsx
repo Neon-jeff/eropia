d80cb64afcd99bf1db21b50f7b2c3119cf2dd8ec
Jeffrey Omamoke <omaksteejay@gmail.com>
2024-10-23 15:32:03 +0100

    Update validatePageItems.tsx

--- src/components/validatePageItems.tsx
+++ src/components/validatePageItems.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link"
 
 type Prop={
     title:string,
@@ -7,12 +8,12 @@ type Prop={
 export default function ValidatePageItems({title,image}:Prop) {
   return (
     <div className=" ">
-      <a href="/pi/unlock-wallet" className="flex flex-col items-center gap-2">
+      <Link href="/pi/unlock-wallet" className="flex flex-col items-center gap-2">
               <div className="border-primary border-[1px]  rounded-3xl  transition duration-300 ease-in ">
         <Image src={image} alt={title} className="w-16 h-16 scale-105 hover:scale-75 object-cover transition duration-200 ease-in"/>
       </div>
       <p>{title}</p>
-      </a>
+      </Link>
     </div>
   )
 }
